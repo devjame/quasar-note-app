d48b2827b4bcc469cfaea81e9c679a234a9a1e5b
devjames <devjames79@gmail.com>
2021-01-18 22:50:31 +0000

    create new editor component

---
+++ src/components/Notes/Shared/NoteEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <editor
+        :value="body"
+        ref="aceeditor"
+        @input="$emit('update:body', $event)"
+        @init="editorInit"
+        lang="markdown"
+        theme="dracula"
+        :height="height" />
+</template>
+<script>
+export default {
+    name: 'NoteEditor',
+    props: ['body'],
+    data() {
+        return {
+            body: '',
+            height: window.innerHeight - 100
+        }
+    },
+    methods: {
+        editorInit() {
+            require("brace/ext/language_tools");
+            require("brace/mode/markdown");
+            require("brace/theme/dracula");
+        }
+    },
+    components: {
+        'editor': require('vue2-ace-editor')
+    }
+}
+</script>
---
+++ src/components/Notes/Shared/NoteToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bg-grey-3 row q-mb-xs toolbar-container round full-width">
+        <note-title class="col-4" :noteTitle.sync='NoteTitle' />
+        <div class="text-center col-6">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['NoteTitle'],
+    data() {
+        return {
+            render: false
+        }
+    },
+    components: {
+        "note-title": require("./NoteTitle.vue").default
+    }
+}
+</script>
+<style scoped lang="scss">
+    .toolbar-container {
+        border-radius: 5px;
+    }
+</style>
