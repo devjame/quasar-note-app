25889cbbe1cb50f11b861a71cb320e5538b9280c
devjames <devjames79@gmail.com>
2021-01-18 22:50:57 +0000

    create new home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="q-pa-md">
+      <div class="absolute-center text-center">
+        <p></p>
+        <q-btn
+          to="/new_note"
+          color="primary"
+          class="q-mb-md"
+          outline
+          label="New Note" />
+        <p class="text-grey-7">Ou selecione uma nota ao lado para editor.</p>
+      </div>
+  </q-page>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+
+}
+</script>
