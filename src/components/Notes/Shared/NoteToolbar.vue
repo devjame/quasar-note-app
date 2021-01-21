d85596afe2ef8d75a3a855084b53742d552b3e53
devjames <devjames79@gmail.com>
2021-01-21 16:54:08 +0000

    add the preview and save button and its func

--- src/components/Notes/Shared/NoteToolbar.vue
+++ src/components/Notes/Shared/NoteToolbar.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="bg-grey-3 row q-mb-xs toolbar-container round full-width">
+    <div 
+        class="bg-grey-3 row relative-position q-mb-xs toolbar-container round full-width">
         <note-title class="col-4" :noteTitle.sync='NoteTitle' />
-        <div class="text-center col-6">
+        <div class="text-center absolute-right q-ma-md">
+            <q-btn
+                @click="$emit('saveNote')"
+                dense
+                round
+                color="primary"
+                icon='check'
+                class="q-mr-md" />
+            <q-btn
+              dense
+              @click="toogleRender"
+              color="white"
+              text-color="black">
+                {{ renderText }}
+              </q-btn>
         </div>
     </div>
 </template>
@@ -10,7 +25,20 @@ export default {
     props: ['NoteTitle'],
     data() {
         return {
-            render: false
+            renderText: 'Preview'
+        }
+    },
+    methods: {
+        toogleRender() {
+            let render = this.$store.getters.preview
+            if (!render) {
+                this.renderText = 'Preview'
+                this.$store.dispatch('tooglePreview', true)
+            }
+            else {
+                this.renderText = 'Editor'
+                this.$store.dispatch('tooglePreview', false)
+            }
         }
     },
     components: {
