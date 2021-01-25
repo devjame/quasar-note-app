549a30e6404fba4343458f92400e8046714cc23c
devjames <devjames79@gmail.com>
2021-01-25 18:57:35 +0000

    remove the ace-editor directive

--- src/boot/ace-editor.js
+++
@@ -1,9 +0,0 @@
-// import something here
-import Vue from 'vue'
-import VueDirectiveMarkdown from 'vue-directive-markdown'
-// "async" is optional;
-// more info on params: https://quasar.dev/quasar-cli/boot-files
-export default async (/* { app, router, Vue ... } */) => {
-  // something to do
-  Vue.directive('decode', VueDirectiveMarkdown)
-}
