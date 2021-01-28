9d620dc9641e47ef704977bc9f12bd53805657f8
devjames <devjames79@gmail.com>
2021-01-28 22:07:25 +0000

    added api endpoint

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,9 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+
+const api = axios.create({baseURL: 'http://127.0.0.1:5000'})
+
+Vue.prototype.$api = api
+
+export { axios, api}
